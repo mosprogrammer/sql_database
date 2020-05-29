d9889838c7a6869b1b4acdc1f9a55057451c2511
mosukung <satawat.rattha@gmail.com>
2020-05-29 12:28:18 +0700

    First commit

---
+++ main.sql
@@ -0,0 +1,14 @@
+-- สร้าง Database
+-- CREATE DATABASE testSql;
+
+-- เรียกใช้งาน database testSql
+USE testSql;
+
+-- สร้าง table user
+-- CREATE TABLE users(
+--     id integer NOT NULL PRIMARY KEY AUTO_INCREMENT
+-- );
+
+SHOW TABLES;
+
+SHOW COLUMNS FROM users;
