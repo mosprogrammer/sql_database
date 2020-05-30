ea4c343271c6dd17086afd90c40eeaa8797396cb
Suphawadee <ked.suphawadee@gmail.com>
2020-05-30 14:37:10 +0700

    test create user

--- main.sql
+++ main.sql
@@ -1,1 +1,33 @@
 -- ทดสอบ
+-- สร้าง DB
+-- CREATE DATABASE eba;
+
+use eba;
+
+-- CREATE TABLE users (
+--   id integer NOT NULL AUTO_INCREMENT,
+--   firstname varchar(50),
+--   lastname varchar(50),
+--   email varchar(100),
+--   password varchar(60),
+--   user_avatar varchar(50),
+--   user_cover varchar(50),
+--   PRIMARY KEY (id)
+-- );
+-- ลบตาราง
+-- DROP TABLE users;
+
+-- เพิ่ม field birthday
+-- ALTER TABLE users
+-- ADD birthday DATE;
+
+-- แก้ไข field email
+-- ALTER TABLE users
+-- MODIFY email VARCHAR(250);
+
+-- CREATE TABLE follows (
+--   id integer NOT NULL AUTO_INCREMENT PRIMARY KEY,
+--   following integer,
+--   follower integer
+-- );
+
