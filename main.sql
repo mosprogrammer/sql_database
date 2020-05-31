ef685ee494ae3894bf64b4d0a6b66d7cdd5249be
Arnon kijlerdphon <snappy.kop@gmail.com>
2020-05-31 15:46:28 +0700

    add command sql

--- main.sql
+++ main.sql
@@ -1,24 +1,88 @@
--- สร้าง Database
--- CREATE DATABASE testSql;
--- เรียกใช้งาน database testSql
-USE testSql;
--- สร้าง table user
--- CREATE TABLE users(
---     id integer NOT NULL PRIMARY KEY AUTO_INCREMENT
--- );
---
--- แสดง table ทั้งหมดใน testSql
---
-SHOW TABLES;
---
--- เพิ่ม column username ใน users
---
-ALTER TABLE users
-ADD username VARCHAR(50) UNIQUE NOT NULL;
---
--- แสดง column ทั้งหมดใน users
---
-SHOW COLUMNS
-FROM users;
---
--- ทดสอบ
+-- สร้าง users table
+CREATE TABLE `users` (
+  `id` int AUTO_INCREMENT,
+  `first_name` varchar(100),
+  `last_name` varchar(100),
+  `password` int(12),
+  `profile_image` varchar(255),
+  -- กำหนด primary key ให้กับ id
+  PRIMARY KEY (id)
+);
+
+-- สร้าง user_logins table
+CREATE TABLE `user_logins` (
+  `id` int AUTO_INCREMENT,
+  `userId` int,
+  `login_date` datetime,
+  -- กำหนด primary key ให้กับ id
+  PRIMARY KEY (id),
+  -- กำหนด foreign key ให้ userId โดยอิงจาก users.id table
+  FOREIGN KEY (`userId`) REFERENCES `users` (`id`)
+);
+
+-- สร้าง verification table
+CREATE TABLE `verification` (
+  `id` int AUTO_INCREMENT,
+  `token` int,
+  `userId` int,
+  `token_expire` datetime,
+  -- กำหนด primary key ให้กับ id
+  PRIMARY KEY (id),
+  -- กำหนด foreign key ให้ userId โดยอิงจาก users.id table
+  FOREIGN KEY (`userId`) REFERENCES `users` (`id`)
+);
+
+-- สร้าง feeds table
+CREATE TABLE `feeds` (
+  `id` int AUTO_INCREMENT,
+  `content` varchar(150),
+  `create_date` datetime,
+  `status` int(1) DEFAULT 1,
+  `share` int(1) DEFAULT 0,
+  `clapId` int,
+  -- กำหนด primary key ให้กับ id
+  PRIMARY KEY (id)
+);
+
+-- สร้าง claps table
+CREATE TABLE `claps` (
+  `id` int AUTO_INCREMENT,
+  `create_date` datetime,
+  `feedId` int,
+  `userId` int,
+  -- กำหนด primary key ให้กับ id
+  PRIMARY KEY (id),
+  -- กำหนด foreign key ให้ userId โดยอิงจาก users.id table
+  FOREIGN KEY (`userId`) REFERENCES `users` (`id`),
+  -- กำหนด foreign key ให้ id โดยอิงจาก feeds.clapId table
+  FOREIGN KEY (`id`) REFERENCES `feeds` (`clapId`),
+  -- กำหนด foreign key ให้ feedId โดยอิงจาก feeds.id table
+  FOREIGN KEY (`feedId`) REFERENCES `feeds` (`id`)
+);
+
+-- สร้าง comments table
+CREATE TABLE `comments` (
+  `id` int AUTO_INCREMENT,
+  `create_date` datetime,
+  `detail` varchar(150),
+  `userId` int,
+  -- กำหนด primary key ให้กับ id
+  PRIMARY KEY (id),
+  -- กำหนด foreign key ให้ userId โดยอิงจาก users.id table
+  FOREIGN KEY (`userId`) REFERENCES `users` (`id`)
+);
+
+-- สร้าง addresses table
+CREATE TABLE `addresses` (
+  `userId` int,
+  `line_1_building` varchar(150),
+  `line_2_street` varchar(150),
+  `province` varchar(50),
+  `county` varchar(50),
+  `district` varchar(50),
+  `zip_postcode` int(5),
+  -- กำหนด primary key ให้กับ id
+  PRIMARY KEY (id),
+  -- กำหนด foreign key ให้ userId โดยอิงจาก users.id table
+  FOREIGN KEY (`userId`) REFERENCES `users` (`id`)
+);
