2e4489cc68198ad4309a64eec7723b6be23a33e4
comdevx <comdevx@gmail.com>
2020-06-01 10:41:53 +0700

    add file

--- main.sql
+++ main.sql
@@ -2,6 +2,7 @@
 -- CREATE DATABASE testSql;
 -- เรียกใช้งาน database testSql
 USE testSql;
+
 -- สร้าง table user
 -- CREATE TABLE users(
 --     id integer NOT NULL PRIMARY KEY AUTO_INCREMENT
@@ -10,15 +11,75 @@ USE testSql;
 -- แสดง table ทั้งหมดใน testSql
 --
 SHOW TABLES;
+
 --
--- เพิ่ม column username ใน users
+-- เพิ่ม column id, email, firstname, lastname, created_at, updated_at ใน users
 --
-ALTER TABLE users
-ADD username VARCHAR(50) UNIQUE NOT NULL;
+CREATE TABLE `users` (
+  `id` varchar(255) PRIMARY KEY,
+  `email` varchar(255),
+  `firstname` varchar(255),
+  `lastname` varchar(255),
+  `created_at` datetime,
+  `updated_at` datetime
+);
+
 --
--- แสดง column ทั้งหมดใน users
+-- เพิ่ม column id, following, follower, created_at ใน follows
 --
-SHOW COLUMNS
-FROM users;
+CREATE TABLE `follows` (
+  `id` varchar(255) PRIMARY KEY,
+  `following` array,
+  `follower` array,
+  `created_at` datetime
+);
+
 --
--- ทดสอบ
+-- เพิ่ม column id, message, user_id, deleted, like_total, comment_total, created_at, updated_at ใน posts
+--
+CREATE TABLE `posts` (
+  `id` varchar(255) PRIMARY KEY,
+  `message` varchar(255),
+  `user_id` varchar(255),
+  `deleted` boolean,
+  `like_total` integer,
+  `comment_total` integer,
+  `created_at` datetime,
+  `updated_at` datetime
+);
+
+--
+-- เพิ่ม column id, post_id, user_id ใน likes
+--
+CREATE TABLE `likes` (
+  `id` varchar(255) PRIMARY KEY,
+  `post_id` varchar(255),
+  `user_id` varchar(255)
+);
+
+--
+-- เพิ่ม column id, post_id, user_id ใน comments
+--
+CREATE TABLE `comments` (
+  `id` varchar(255) PRIMARY KEY,
+  `post` varchar(255),
+  `user_id` varchar(255)
+);
+
+--
+-- เพิ่ม column id, post_id, user_id ใน shares
+--
+CREATE TABLE `shares` (
+  `id` varchar(255) PRIMARY KEY,
+  `post_id` varchar(255),
+  `user_id` varchar(255)
+);
+
+--
+-- เพิ่ม column id, post_id, user_id ใน post_hide
+--
+CREATE TABLE `post_hide` (
+  `id` varchar(255) PRIMARY KEY,
+  `post_id` varchar(255),
+  `user_id` varchar(255)
+);
