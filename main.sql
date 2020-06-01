593b57b53d8d0c2462c1b0d8cb8f287b52e6109f
comdevx <comdevx@gmail.com>
2020-06-01 10:46:11 +0700

    update length and type

--- main.sql
+++ main.sql
@@ -16,10 +16,10 @@ SHOW TABLES;
 -- เพิ่ม column id, email, firstname, lastname, created_at, updated_at ใน users
 --
 CREATE TABLE `users` (
-  `id` varchar(255) PRIMARY KEY,
-  `email` varchar(255),
-  `firstname` varchar(255),
-  `lastname` varchar(255),
+  `id` varchar(32) PRIMARY KEY UNIQUE NOT NULL,
+  `email` varchar(50),
+  `firstname` varchar(50),
+  `lastname` varchar(50),
   `created_at` datetime,
   `updated_at` datetime
 );
@@ -28,7 +28,7 @@ CREATE TABLE `users` (
 -- เพิ่ม column id, following, follower, created_at ใน follows
 --
 CREATE TABLE `follows` (
-  `id` varchar(255) PRIMARY KEY,
+  `id` varchar(32) PRIMARY KEY UNIQUE NOT NULL,
   `following` array,
   `follower` array,
   `created_at` datetime
@@ -38,9 +38,9 @@ CREATE TABLE `follows` (
 -- เพิ่ม column id, message, user_id, deleted, like_total, comment_total, created_at, updated_at ใน posts
 --
 CREATE TABLE `posts` (
-  `id` varchar(255) PRIMARY KEY,
+  `id` varchar(32) PRIMARY KEY UNIQUE NOT NULL,
   `message` varchar(255),
-  `user_id` varchar(255),
+  `user_id` varchar(32),
   `deleted` boolean,
   `like_total` integer,
   `comment_total` integer,
@@ -52,34 +52,34 @@ CREATE TABLE `posts` (
 -- เพิ่ม column id, post_id, user_id ใน likes
 --
 CREATE TABLE `likes` (
-  `id` varchar(255) PRIMARY KEY,
-  `post_id` varchar(255),
-  `user_id` varchar(255)
+  `id` varchar(32) PRIMARY KEY UNIQUE NOT NULL,
+  `post_id` varchar(32),
+  `user_id` varchar(32)
 );
 
 --
 -- เพิ่ม column id, post_id, user_id ใน comments
 --
 CREATE TABLE `comments` (
-  `id` varchar(255) PRIMARY KEY,
-  `post` varchar(255),
-  `user_id` varchar(255)
+  `id` varchar(32) PRIMARY KEY UNIQUE NOT NULL,
+  `post` varchar(32),
+  `user_id` varchar(32)
 );
 
 --
 -- เพิ่ม column id, post_id, user_id ใน shares
 --
 CREATE TABLE `shares` (
-  `id` varchar(255) PRIMARY KEY,
-  `post_id` varchar(255),
-  `user_id` varchar(255)
+  `id` varchar(32) PRIMARY KEY UNIQUE NOT NULL,
+  `post_id` varchar(32),
+  `user_id` varchar(32)
 );
 
 --
 -- เพิ่ม column id, post_id, user_id ใน post_hide
 --
 CREATE TABLE `post_hide` (
-  `id` varchar(255) PRIMARY KEY,
-  `post_id` varchar(255),
-  `user_id` varchar(255)
+  `id` varchar(32) PRIMARY KEY UNIQUE NOT NULL,
+  `post_id` varchar(32),
+  `user_id` varchar(32)
 );
