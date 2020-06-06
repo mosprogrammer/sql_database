4b5aa1f535dc2d3c62b9dff3078b9910531ee724
Arnon kijlerdphon <snappy.kop@gmail.com>
2020-06-06 14:56:41 +0700

    add userId in feeds table

--- ichamp/main.sql
+++ ichamp/main.sql
@@ -2,3 +2,8 @@
 CREATE DATABASE eba;
 -- เลือกใช้ eba db
 USE eba;
+
+-- Count จำนวน Post ของ User
+SELECT COUNT(column_name)
+FROM table_name
+WHERE condition;
--- ichamp/tables/feeds.sql
+++ ichamp/tables/feeds.sql
@@ -1,21 +1,24 @@
 -- สร้าง feeds table
 CREATE TABLE `feeds` (
   `id` int AUTO_INCREMENT,
+  `userId` int,
   `content` varchar(150),
   `create_date` datetime,
   `status` int(1) DEFAULT 1,
   `share` int(1) DEFAULT 0,
   -- กำหนด primary key ให้กับ id
-  PRIMARY KEY (id)
+  PRIMARY KEY (id),
+  -- กำหนด foreign key ให้ userId โดยอิงจาก users.id table
+  FOREIGN KEY (`userId`) REFERENCES `users` (`id`)
 );
 
 -- เพิ่มข้อมูล
-INSERT INTO `feeds` (`id`, `content`, `create_date`)
-VALUES (1, "Lorem Ipsum has been", "2010-05-17 22:52:21"),
-  (2, "when an unknown printer took", "2010-05-20 22:52:21"),
-  (3, "PageMaker including versions of Lorem Ipsum.", "2010-03-02 22:52:21"),
-  (4, "Various versions have evolved over the years", "2010-04-17 22:52:21"),
-  (5, "There are many variations of passages of Lorem Ipsum", "2010-10-15 22:52:21");
+INSERT INTO `feeds` (`id`, `userId`, `content`, `create_date`)
+VALUES (1, `23`,"Lorem Ipsum has been", "2010-05-17 22:52:21"),
+  (2, `20`,"when an unknown printer took", "2010-05-20 22:52:21"),
+  (3, `20`,"PageMaker including versions of Lorem Ipsum.", "2010-03-02 22:52:21"),
+  (4, `10`,"Various versions have evolved over the years", "2010-04-17 22:52:21"),
+  (5, `18`,"There are many variations of passages of Lorem Ipsum", "2010-10-15 22:52:21");
 
 -- แก้ไข
 UPDATE `feeds`
