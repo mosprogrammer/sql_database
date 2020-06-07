efdb18fa87f29782fa43cd60fab1960a988d5f59
Teem <teem@Maruttheps-MacBook-Pro.local>
2020-06-08 01:07:10 +0700

    let go

---
+++ homework.sql
@@ -0,0 +1,23 @@
+-- 1.เขียน SQL เพื่อนำไปใช้สร้างหน้า Feed ใน App eBa โดยมีเงื่อนไขว่า
+-- จะต้องแสดงเฉพาะ Post ของเรา และของเพื่อนที่เราได้ทำการ Follow แล้วเท่านั้น 
+-- แต่ถ้า Post ของเราถูกลบออกไป จะต้องไม่แสดงในหน้า Feed
+
+
+-- 2. ในแต่ละ Post ที่แสดงในหน้า Feed จะต้องมีจำนวน Clap Comment และ Share
+
+
+-- 3. เขียน SQL สำหรับ Clap Comemnt Share และซ่อน Post
+-- 3.1 Clap Comemnt
+UPDATE `comments` 
+SET `comment_count` = `comment_count` + 1
+WHERE `postid` = 10
+
+-- 3.2 Clap Share
+UPDATE `shares` 
+SET `share_count` = `share_count` + 1
+WHERE `postid` = 20
+
+-- 3.3 Hide Post
+-- (`hideid`, `feedid`, `postid`) 
+INSERT INTO `hide` VALUES
+(null,20,10);
--- main.sql
+++ main.sql
@@ -1,82 +1,205 @@
+-- CREATE DATABASE eba_db;
 
--- CREATE DATABASE teem;
--- DROP DATABASE databasename;
+-- SELECT * FROM posts;
 
-USE teem;
+-- สร้าง table 
+
+-- CREATE TABLE `users` (
+--   `userid` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+--   `name` VARCHAR(50) NOT NULL,
+--   `email` varchar(50) DEFAULT NULL
+-- );
+
+-- CREATE TABLE `follow` (
+--   `followid` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+--   `follower_id` int(11) NOT NULL,
+--   `following_id` int(11) NOT NULL
+-- );
+
+-- ALTER TABLE `follow`
+--   ADD CONSTRAINT `fk_comments_user` FOREIGN KEY (`follower_id`) REFERENCES `users` (`userid`),
+--   ADD CONSTRAINT `fk_comments_post` FOREIGN KEY (`following_id`) REFERENCES `users` (`userid`);
+
+-- CREATE TABLE `comments` (
+--   `commentid` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+--   `userid` int(11) NOT NULL,
+--   `postid` int(11) NOT NULL,
+--   `content_comment` text NOT NULL
+--   `comment_count` int(11) DEFAULT 0
+-- );
+
+-- ALTER TABLE `comments`
+--   ADD CONSTRAINT `fk_comm_user` FOREIGN KEY (`userid`) REFERENCES `users` (`userid`),
+--   ADD CONSTRAINT `fk_comm_post` FOREIGN KEY (`postid`) REFERENCES `posts` (`postid`);
+
+-- CREATE TABLE `feeds` (
+--   `feedid` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+--   `userid` int(11) NOT NULL,
+--   `postid` int(11) NOT NULL,
+--   `followid` int(11) NOT NULL
+-- );
+
+-- ALTER TABLE `feeds`
+--   ADD CONSTRAINT `fk_feeds_user` FOREIGN KEY (`userid`) REFERENCES `users` (`userid`),
+--   ADD CONSTRAINT `fk_feeds_follow` FOREIGN KEY (`followid`) REFERENCES `follow` (`followid`),
+--   ADD CONSTRAINT `fk_feeds_post` FOREIGN KEY (`postid`) REFERENCES `posts` (`postid`);
+
+-- CREATE TABLE `shares` (
+--   `shareid` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+--   `feedid` int(11) NOT NULL,
+--   `postid` int(11) NOT NULL,
+--   `share_count` int(11) DEFAULT 0
+-- );
+
+-- ALTER TABLE `shares`
+--   ADD CONSTRAINT `fk_shares_feed` FOREIGN KEY (`feedid`) REFERENCES `feeds` (`feedid`),
+--   ADD CONSTRAINT `fk_shares_post` FOREIGN KEY (`postid`) REFERENCES `posts` (`postid`);
+
+-- CREATE TABLE `hide` (
+--   `hideid` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+--   `feedid` int(11) NOT NULL,
+--   `postid` int(11) NOT NULL
+-- );
+
+-- ALTER TABLE `hide`
+--   ADD CONSTRAINT `fk_hide_feed` FOREIGN KEY (`feedid`) REFERENCES `feeds` (`feedid`),
+--   ADD CONSTRAINT `fk_hide_post` FOREIGN KEY (`postid`) REFERENCES `posts` (`postid`);
+
+-- CREATE TABLE `posts` (
+--   `postid` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+--   `userid` int(11) NOT NULL,
+--   `content_post` text NOT NULL,
+--   `clap_count` int(11) DEFAULT 0
+-- );
+
+-- ALTER TABLE `posts`
+--   ADD CONSTRAINT `fk_posts_user` FOREIGN KEY (`userid`) REFERENCES `users` (`userid`);
+
+-- CREATE TABLE `claps` (
+--   `clapid` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+--   `postid` int(11) NOT NULL,
+--   `userid` int(11) NOT NULL
+-- );
+
+-- ALTER TABLE `claps`
+--   ADD CONSTRAINT `fk_claps_user` FOREIGN KEY (`userid`) REFERENCES `users` (`userid`),
+--   ADD CONSTRAINT `fk_claps_post` FOREIGN KEY (`postid`) REFERENCES `posts` (`postid`);
 
 
--- DROP TABLE table_name;
 
--- สร้างตารางเก็บข้อมูลสมาชิก 
-CREATE TABLE users(
-    id integer NOT NULL PRIMARY KEY, 
-    email varchar(50), 
-    pass varchar(30), 
-    firstname varchar(50), 
-    lastname varchar(50), 
-    profile_Image varchar(50), 
-    archivement varchar(255), 
-    address_id integer 
-);
--- สร้างตารางเก็บข้อมูลที่อยู่
-CREATE TABLE address (
-    id integer(10) NOT NULL PRIMARY KEY, 
-    province varchar(50), 
-    district varchar(50), 
-    amphur varchar(50) 
-);
--- สร้างตารางเก็บข้อมูลยืนยันตัวตน
-CREATE TABLE verify(
-    id integer(10) NOT NULL PRIMARY KEY, 
-    token_Expire integer(10),
-    varify_status varchar(50), 
-    users_id integer(10) 
-);
--- สร้างตารางเก็บข้อมูลผู้ติดตาม
-CREATE TABLE follow(
-    id integer(10) NOT NULL PRIMARY KEY, 
-    follower integer(10), 
-    following integer(10) 
-);
--- สร้างตารางเก็บข้อมูลโพสต์
-CREATE TABLE posts(
-    id integer(10) NOT NULL PRIMARY KEY, 
-    users_id integer(10), 
-    content text, 
-    date_Time DateTime 
-);
--- สร้างตารางLike
-CREATE TABLE claps(
-    id integer(10) NOT NULL PRIMARY KEY, 
-    posts_id integer(10), 
-    users_id integer(10) 
-);
--- สร้างตารางคอมเม้น
-CREATE TABLE comment(
-    id integer(10) NOT NULL PRIMARY KEY, 
-    content text,
-    date_Time DateTime, 
-    posts_id integer(10), 
-    users_id integer(10) 
-);
--- สร้างตารางฟีต
-CREATE TABLE feeds(
-    id integer(10) NOT NULL PRIMARY KEY, 
-    posts_id integer(10),
-    users_id integer(10) 
-);
--- สร้างตารางแชร์
-CREATE TABLE share_posts(
-  id integer(10) NOT NULL PRIMARY KEY, 
-  posts_di integer(10), 
-  users_id integer(10), 
-  feeds_id integer(10), 
-  share_Time DateTime
-);
--- สร้างตารางซ่อนข้อมูล
-CREATE TABLE hides (
-  id integer(10) NOT NULL PRIMARY KEY, 
-  users_id integer(10), 
-  posts_id integer(10) 
-);
+-- เพิ่มข้อมูล user 
+-- INSERT INTO `users` VALUES 
+-- (null,'marutthep','test1@test.com'),
+-- (null,'JoJo','test2@test.com'),
+-- (null,'worawit','test3@test.com'),
+-- (null,'somchai','test4@test.com'),
+-- (null,'somyeng','test5@test.com');
+
+
+--เพิ่มข้อมูล follow
+-- INSERT INTO `follow` VALUES
+-- (null,1,2),
+-- (null,2,1);
+
+
+--เพิ่มข้อมูล post
+-- (`postid`, `userid`, `content`, `clap_count`) 
+-- INSERT INTO `posts` VALUES
+-- (null, 1,'Number0',0),
+-- (null, 2,'Number1',0),
+-- (null, 1,'Number2',0),
+-- (null, 2,'Number3',0),
+-- (null, 1,'Number4',0),
+-- (null, 2,'Number5',0),
+-- (null, 1,'Number6',0),
+-- (null, 2,'Number7',0),
+-- (null, 1,'Number8',0),
+-- (null, 2,'Number9',0),
+-- (null, 1,'Number10',0),
+-- (null, 2,'Number11',0),
+-- (null, 1,'Number12',0),
+-- (null, 2,'Number13',0),
+-- (null, 1,'Number14',0),
+-- (null, 2,'Number15',0),
+-- (null, 1,'Number16',0),
+-- (null, 2,'Number17',0),
+-- (null, 1,'Number18',0),
+-- (null, 2,'Number19',0),
+-- (null, 1,'Number20',0);
+
+
+-- (`clapid`, `postid`, `userid`) 
+-- INSERT INTO `claps`  VALUES
+-- (null, 1, 1),
+-- (null, 1, 2),
+-- (null, 1, 3),
+-- (null, 1, 4),
+-- (null, 1, 5),
+-- (null, 2, 1),
+-- (null, 2, 2),
+-- (null, 2, 3),
+-- (null, 2, 4),
+-- (null, 3, 1),
+-- (null, 3, 2),
+-- (null, 4, 1),
+-- (null, 4, 2),
+-- (null, 5, 1),
+-- (null, 5, 2),
+-- (null, 6, 1),
+-- (null, 6, 2),
+-- (null, 7, 1),
+-- (null, 7, 2),
+-- (null, 8, 5);
+
+USE eba_db;
+-- (`commentid`, `user_id`, `post_id`, `content`, `comment_count`) 
+INSERT INTO `comments` VALUES
+(null, 1, 1, 'Hello1',0),
+(null, 2, 1, 'Hello2',0),
+(null, 3, 1, 'Hello3',0),
+(null, 4, 2, 'Hello4',0),
+(null, 5, 2, 'Hello5',0),
+(null, 1, 3, 'Hello6',0),
+(null, 2, 3, 'Hello7',0),
+(null, 3, 4, 'Hello8',0),
+(null, 4, 5, 'Hello9',0),
+(null, 5, 6, 'Hello10',0);
+
+-- (`feed_id`, `user_id`, `post_id`, `follow_id`) 
+INSERT INTO `feeds` VALUES
+(null, 1, 1, 1),
+(null, 2, 2, 2),
+(null, 1, 1, 2),
+(null, 1, 2, 3),
+(null, 1, 3, 2),
+(null, 1, 4, 1),
+(null, 1, 5, 1),
+(null, 1, 6, 2),
+(null, 1, 7, 2),
+(null, 1, 8, 2),
+(null, 1, 9, 3),
+(null, 1, 10, 3),
+(null, 1, 11, 4),
+(null, 1, 12, 5),
+(null, 1, 13, 5),
+(null, 1, 14, 6),
+(null, 1, 15, 7),
+(null, 1, 16, 7),
+(null, 1, 17, 7),
+(null, 1, 18, 7);
+
+-- (`hideid`, `feedid`, `postid`) 
+INSERT INTO `hide` VALUES
+(null,1,10),
+(null,1,11),
+(null,1,12);
+
+-- (`shareid`, `feedid`, `postid`) 
+INSERT INTO `shares` VALUES
+(null,1,10),
+(null,1,11),
+(null,1,12),
+(null,1,13),
+(null,1,14);
+
 
